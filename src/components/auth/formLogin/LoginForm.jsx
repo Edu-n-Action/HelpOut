9effb934464c3d2df9b417b2b2a4e8bc265c3c29
Wafi Afdi Alfaruqhi <wafiafdi@gmail.com>
2024-02-13 11:40:47 +0700

    add : page login sm signup, component text input

---
+++ src/components/auth/formLogin/LoginForm.jsx
@@ -0,0 +1,21 @@
+"use client"
+import React from 'react'
+import InputTextTransparent from '@/components/common/inputText.js/inputTextTransparent'
+function LoginForm({SetForm=()=>{}, Form=undefined, title="Create an Account"}) {
+  return (
+    <form className='max-w-[600px] w-full flex flex-col items-center'>
+        <div className='max-w-[550px] w-full mt-[60px] mb-[10px]'>
+            <h2 className='font-bold text-2xl'>
+                Login to your account
+            </h2>
+        </div>
+        <InputTextTransparent className={"max-w-[550px]"} placeholder='Email' email={true}/>
+        <InputTextTransparent className={"mt-5 max-w-[550px]"} placeholder='Password' />
+        <button className="bg-[#CEFD4A] py-3 w-full z-[10] text-center text-xl mt-9" href={"/"}>
+            Login
+        </button>
+    </form>
+  )
+}
+
+export default LoginForm
---
+++ src/components/auth/formSignUp/SignupForm.jsx
@@ -0,0 +1,23 @@
+"use client"
+import React from 'react'
+import InputTextTransparent from '@/components/common/inputText.js/inputTextTransparent'
+
+function SignupForm() {
+  return (
+    <form className='max-w-[600px] w-full flex flex-col items-center'>
+        <div className='max-w-[550px] w-full mt-[60px] mb-[10px]'>
+            <h2 className='font-bold text-2xl'>
+                Create an account
+            </h2>
+        </div>
+        <InputTextTransparent className={"max-w-[550px]"} placeholder='Email' email={true}/>
+        <InputTextTransparent className={"mt-5 max-w-[550px]"} placeholder='Password'/>
+        <InputTextTransparent className={"mt-5 max-w-[550px]"} placeholder='Confirm Password'/>
+        <button className="bg-[#CEFD4A] py-3 w-full z-[10] text-center text-xl mt-9" href={"/"}>
+            Sign Up
+        </button>
+    </form>
+  )
+}
+
+export default SignupForm
