9cd83db65c0bf176bc5eea5bc4cf32cbe4b7d3b3
mie-intel <paryapradhanika@gmail.com>
2024-02-14 20:31:45 +0700

    feat: materials and attendee

--- src/app/personal/community/event/[id]/page.js
+++ src/app/personal/community/event/[id]/page.js
@@ -3,7 +3,7 @@ import Menu from "./menu";
 
 const EventHome = () => {
   return (
-    <div className="w-full h-fit bg-back flex flex-col items-center">
+    <div className="w-full h-fit min-h-screen bg-back flex flex-col items-center">
       <div className="w-full md:w-[33%] bg-back">
         <EventProfile />
 
