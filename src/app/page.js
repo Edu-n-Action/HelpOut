6316fdddb6ae1ca0992f2ef6a6b2a5fefeae8104
mie-intel <paryapradhanika@gmail.com>
2024-02-19 11:17:16 +0700

    fix: root page

--- src/app/page.js
+++ src/app/page.js
@@ -5,31 +5,31 @@ import ConfirmPopUp from "@/components/common/confirm/confirmPopUp";
 
 export default function Home() {
   return (
-    <main className="relative w-full flex justify-center">
+    <main className="relative aspect-[1280/720] w-full flex justify-center">
       <div className="hidden md:inline absolute md:aspect-[1440/465] w-[100%] bottom-0 z-0">
         <Image srcset={"/ornaments/wavy-low-deks.svg"} alt={"wave"} fill />
       </div>
       <div className="inline md:hidden absolute aspect-[430/550] w-full bottom-0 z-0">
         <Image srcset={"/ornaments/wave-mobile.svg"} alt={"wave"} fill />
       </div>
-      <div className="aspect-[430/932] h-screen max-md:w-full flex flex-col items-center relative pt-[50px] ">
-        <div className="bg-[#2F3A47] rounded-xl p-3 flex justify-center max-w-fit z-[10] ">
-          <p className="text-5xl font-bold">
+      <div className="aspect-[430/932] h-screen w-full md:w-[45%] flex flex-col items-center relative mt-[40%] md:mt-[12%] ">
+        <div className="bg-[#2F3A47] rounded-[4%/12%] flex justify-center max-w-fit z-10 px-[4%] py-[1%]">
+          <p className="text-[11.1vw] md:text-[3.7vw] font-bold">
             <span className="text-[#CEFD4A]">Help</span>
             <span className="text-white">Out</span>
           </p>
         </div>
 
-        <div className="max-w-[283px] w-full flex flex-col mt-[150px]">
+        <div className="aspect-[150/75] md:aspect-[150/60] w-[50%] flex flex-col justify-between mt-[16%] text-[4.5vw] md:text-[1.5vw]">
           <Link
-            className="bg-[#CEFD4A] py-3 max-w-[283px] w-full z-[10] text-center text-2xl"
+            className="bg-[#CEFD4A] aspect-[150/30] md:aspect-[150/25] rounded-[3%/12%] w-full z-[10] flex justify-center items-center outline-0 text-center"
             href={"/personal/"}
           >
             For Personal
           </Link>
           <Link
+            className="bg-[#CEFD4A] aspect-[150/30] md:aspect-[150/25] rounded-[3%/12%] w-full z-[10] flex justify-center items-center outline-0 text-center"
             href={"/community/"}
-            className="bg-[#CEFD4A] py-3 max-w-[283px] w-full z-[10] mt-4 text-center text-2xl"
           >
             For Community
           </Link>
